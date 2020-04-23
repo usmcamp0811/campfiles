08a229096a02c15cb9be131af8aed7c67c0ee93f
Matthew Camp <usmcamp0811@gmail.com>
2020-04-22 22:03:30 -0500

    fixed aur packages

--- pacman-install-docker.sh
+++ pacman-install-docker.sh
@@ -13,15 +13,39 @@ pacman -S --noconfirm rsync lsd ranger archey3 conky feh nitrogen rofi cmake \
     gcc python-pip ttf-ibm-plex thefuck pandoc zathura zathura-pdf-mupdf ripgrep \
     openssh
 
+git clone https://aur.archlinux.org/i3lock-fancy-git.git \
+&& cd i3lock-fancy-git \
+&& pacman -S --noconfirm imagemagick i3lock \
+&& chown -R aur /build \
+&& su aur -c 'makepkg && makepkg -i --noconfirm' \
+&& pacman -U --noconfirm *.pkg.tar.xz
+
+cd /build 
+sudo -u aur yay -G polybar \
+&& cd polybar \
+&& pacman -S --noconfirm python-sphinx i3-wm libmpdclient \
+&& chown -R aur /build \
+&& su aur -c 'makepkg && makepkg -i --noconfirm' \
+&& pacman -U --noconfirm *.pkg.tar.xz
+
+cd /build
+sudo -u aur yay -G python37 \
+&& cd python37 \
+&& pacman -S --noconfirm tk valgrind bluez-libs mpdecimal llvm gdb xorg-server-xvfb \
+&& chown -R aur /build \
+&& sudo -u aur gpg --recv-keys 2D347EA6AA65421D
+&& su aur -c 'makepkg && makepkg -i --noconfirm' \
+&& pacman -U --noconfirm *.pkg.tar.xz
+
 # install yay so we can get AUR stuff
-su aur -c 'yay -S --noconfirm nerd-fonts-hack i3lock-fancy polybar' 
+# su aur -c 'yay -S --noconfirm nerd-fonts-hack i3lock-fancy polybar' 
 
 pip install powerline-shell neovim jedi tldr.py
 
 pacman -Syu --noconfirm 
 pacman -S --noconfirm pandoc texlive-core nodejs 
 pacman -S --noconfirm npm 
-su aur -c 'yay -S --noconfirm python37' 
+# su aur -c 'yay -S --noconfirm python37' 
 pacman -S --noconfirm xsel
 
 # clean things up for docker
